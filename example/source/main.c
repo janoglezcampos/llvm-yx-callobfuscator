d37d063c2cf3605b63f0359627b6d3342c675ba1
janoglezcampos <janoglezcampos@gmail.com>
2024-01-15 16:23:39 +0100

    Changes to the example, simplification

--- example/source/main.c
+++ example/source/main.c
@@ -9,6 +9,5 @@ int main()
     Sleep(1000);
     MessageBoxA(NULL, "Use this time to check the function stack.", "Break", 0);
 
-    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, 0);
     printf("[INFO] Ended\n");
 }
